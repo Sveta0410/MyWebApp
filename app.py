56ca56224bfeb2eed8d1c1efcb18815d5cf200dd
Sveta <sveta.zheleznova.03@mail.ru>
2021-11-12 22:26:33 +0300

    registratoin without exceptions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -13,21 +13,37 @@ conn = psycopg2.connect(database="service_db",
 cursor = conn.cursor()
 
 
-@app.route('/login/', methods=['GET'])
-def index():
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            if username == '' or password == '':
+                return render_template('login.html', err_empty=1)
+            elif not records:
+                return render_template('login.html', err_not_in_db=2)
+            elif records:
+                return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
     return render_template('login.html')
 
 
-@app.route('/login/', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
 
-    if username == '' or password == '':
-        return render_template('login.html', err_empty=1)
-    elif not records:
-        return render_template('login.html', err_not_in_db=2)
-    elif records:
-        return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
+
+        return redirect('/login/')
+
+    return render_template('registration.html')
